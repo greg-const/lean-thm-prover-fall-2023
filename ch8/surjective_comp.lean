c274e0ca91e7248d4dec0ddd9f76925eb6e8c8d0
greg-const <86847050+greg-const@users.noreply.github.com>
2023-10-27 12:01:21 -0400

    Update surjective_comp.lean

--- ch8/surjective_comp.lean
+++ ch8/surjective_comp.lean
@@ -6,8 +6,23 @@ open function
 universes u v w
 variables {α : Type u} {β : Type v} {γ : Type w}
 open function
-
 lemma surjective_comp {g : β → γ} {f : α → β}
+  (hg : surjective g) (hf : surjective f) :
+surjective (g ∘ f) := 
+begin
+  -- We want to show that for every c in γ, there exists an a in α such that g(f(a)) = c.
+  rintros c,
+  -- Use the surjectivity of g to find some b in β such that g(b) = c.
+  obtain ⟨b, hb⟩ := hg c,
+  -- Use the surjectivity of f to find some a in α such that f(a) = b.
+  obtain ⟨a, ha⟩ := hf b,
+  -- Now, we show that g(f(a)) = c.
+  use a,
+  rw ←hb,
+  rw ←ha,
+end
+
+lemma surjective_comp2{g : β → γ} {f : α → β}
   (hg : surjective g) (hf : surjective f) :
 surjective (g ∘ f) := 
 begin
